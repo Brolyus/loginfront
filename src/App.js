48cf8cde568d73b7ce68d4868cdf189be33112c5
Brolyus <desousanthony@gmail.com>
2020-01-13 17:14:16 +0100

    passing props'

--- src/App.js
+++ src/App.js
@@ -1,22 +1,98 @@
 import React, { Component } from 'react'
-import { Link, Route, Switch } from 'react-router-dom'
+import { Link, Route, Switch, Redirect } from 'react-router-dom'
 import Home from './Home'
 import Secret from './Secret'
+import axios from 'axios'
 
 export default class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            message: 'Loading...',
+            email: '',
+            password: '',
+            token: null,
+            loggedIn: false
+        }
+    }
+    componentDidMount() {
+        fetch('http://localhost:8080/api/home')
+            .then(res => res.text())
+
+            .then(res => this.setState({ message: res }))
+    }
+
+    handleToken = () => {
+        if (this.state.token !== null) {
+            this.setState({
+                loggedIn: true
+            })
+        }
+    }
+
+    handleChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+        axios
+            .post('http://localhost:8080/api/home', {
+                email: this.state.email,
+                password: this.state.password
+            })
+            .then(data =>
+                this.setState({
+                    token: data.data.token
+                })
+            )
+            .then(() => this.handleToken())
+    }
+
     render() {
         return (
-            <div>
+            <div style={{ backgroundColor: 'darkgrey' }}>
                 <ul>
                     <li>
-                        <Link to='/'>Home</Link>
+                        <Link
+                            to={{
+                                pathname: '/',
+                                state: {
+                                    ...this.state,
+                                    handleToken: this.handleToken,
+                                    handleSubmit: this.handleSubmit,
+                                    handleChange: this.handleChange
+                                }
+                            }}
+                        >
+                            Home
+                        </Link>
                     </li>
                     <li>
                         <Link to='/secret'>Secret</Link>
                     </li>
                 </ul>
                 <Switch>
-                    <Route exact path='/' component={Home} />
+                    <Route
+                        exact
+                        path='/'
+                        render={() => (
+                            <Home
+                                {...this.state}
+                                handleToken={this.handleToken}
+                                handleSubmit={this.handleSubmit}
+                                handleChange={this.handleChange}
+                            />
+                        )}
+                    />
+                    {/* {this.state.loggedIn ? (
+                            <Redirect to='/secret' />
+                        ) : (
+                            <Home />
+                        )}
+                    </Route> */}
                     <Route path='/secret' component={Secret} />
                 </Switch>
             </div>
--- src/Home.js
+++ src/Home.js
@@ -1,52 +1,16 @@
 import React, { Component } from 'react'
-import axios from 'axios'
 
 export default class Home extends Component {
-    constructor(props) {
-        super(props)
-        //Set default message
-        this.state = {
-            message: 'Loading...',
-            email: '',
-            password: ''
-        }
-    }
-    componentDidMount() {
-        fetch('http://localhost:8080/api/home')
-            .then(res => res.text())
-
-            .then(res => this.setState({ message: res }))
-    }
-
-    handleChange = event => {
-        this.setState({
-            [event.target.name]: event.target.value
-        })
-    }
-
-    handleSubmit = event => {
-        event.preventDefault()
-        axios
-            .post('http://localhost:8000/home', {
-                email: this.state.email,
-                password: this.state.password
-            })
-            .then(response => {
-                console.log('yes!', response.data)
-            })
-    }
-    //essayer avec des données en dur
-
     render() {
         return (
             <div>
                 <h1>Home</h1>
-                <p>{this.state.message}</p>
+                <p>{this.props.message}</p>
                 <br />
                 <div className='LoginForm'>
                     <h1>CONNEXION</h1>
 
-                    <form onSubmit={this.handleSubmit}>
+                    <form onSubmit={this.props.handleSubmit} action='#'>
                         <fieldset>
                             <div className='form-data'>
                                 <label htmlFor='email'>E-MAIL</label>
@@ -55,8 +19,8 @@ export default class Home extends Component {
                                     id='email'
                                     placeholder='Enter your email'
                                     name='email'
-                                    onChange={this.handleChange}
-                                    value={this.state.email}
+                                    onChange={this.props.handleChange}
+                                    value={this.props.email}
                                 />
                             </div>
 
@@ -67,8 +31,8 @@ export default class Home extends Component {
                                     id='password'
                                     placeholder='Enter your password'
                                     name='password'
-                                    onChange={this.handleChange}
-                                    value={this.state.password}
+                                    onChange={this.props.handleChange}
+                                    value={this.props.password}
                                     required
                                 />
                             </div>
