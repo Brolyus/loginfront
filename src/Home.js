2dcae407072d8704f9bb0079ee76f6fed7919f55
Brolyus <desousanthony@gmail.com>
2020-01-13 15:46:27 +0100

    ajout formulaire de log

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class Home extends Component {
     constructor(props) {
         super(props)
         //Set default message
         this.state = {
-            message: 'Loading...'
+            message: 'Loading...',
+            email: '',
+            password: ''
         }
     }
     componentDidMount() {
@@ -14,11 +17,69 @@ export default class Home extends Component {
 
             .then(res => this.setState({ message: res }))
     }
+
+    handleChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+        axios
+            .post('http://localhost:8000/home', {
+                email: this.state.email,
+                password: this.state.password
+            })
+            .then(response => {
+                console.log('yes!', response.data)
+            })
+    }
+    //essayer avec des données en dur
+
     render() {
         return (
             <div>
                 <h1>Home</h1>
                 <p>{this.state.message}</p>
+                <br />
+                <div className='LoginForm'>
+                    <h1>CONNEXION</h1>
+
+                    <form onSubmit={this.handleSubmit}>
+                        <fieldset>
+                            <div className='form-data'>
+                                <label htmlFor='email'>E-MAIL</label>
+                                <input
+                                    type='text'
+                                    id='email'
+                                    placeholder='Enter your email'
+                                    name='email'
+                                    onChange={this.handleChange}
+                                    value={this.state.email}
+                                />
+                            </div>
+
+                            <div className='form-data'>
+                                <label htmlFor='password'>PASSWORD</label>
+                                <input
+                                    type='password'
+                                    id='password'
+                                    placeholder='Enter your password'
+                                    name='password'
+                                    onChange={this.handleChange}
+                                    value={this.state.password}
+                                    required
+                                />
+                            </div>
+
+                            <hr />
+                            <div className='form-data'>
+                                <input type='submit' value='Login' />
+                            </div>
+                        </fieldset>
+                    </form>
+                </div>
             </div>
         )
     }
